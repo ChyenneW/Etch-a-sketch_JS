1cc86fa4c87e144eafeee04783afe2602b0b4606
ChyenneW <williamscheyenne771@gmail.com>
2022-08-09 05:34:20 -0500

    Display div from js

---
+++ index.js
@@ -0,0 +1,8 @@
+const sketchPad = document.getElementById("sketchPad");
+
+const sketchPixel = document.createElement('div');
+sketchPixel.setAttribute('style', 'border:5px solid black; background-color: red;');
+
+
+
+sketchPad.appendChild(sketchPixel);
