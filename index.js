386f65a20c0c1a71759b464d56938e5cd16f8c0b
ChyenneW <williamscheyenne771@gmail.com>
2022-08-10 09:17:25 -0500

    Display sketch pad from js

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const sketchPad = document.getElementById('sketchPad');
-
-const sketchPixel = document.createElement('div');
-sketchPixel.setAttribute('style', 'border:5px solid black; background-color: red;');
-
 const sliderValue = document.getElementById('slider');
 const gridSliderCount = document.getElementById('gridSliderCount');
 gridSliderCount.textContent = `${sliderValue.value} x ${sliderValue.value}`;
 
-
 sliderValue.addEventListener('change', (event) => {
+    console.log(sliderValue.value);
     gridSliderCount.textContent = `${sliderValue.value} x ${sliderValue.value}`;
+    changePixelPad();
 });
 
-sketchPad.appendChild(sketchPixel);
+const sketchPad = document.getElementById('sketchPad');
+
+function changePixelPad() {
+    sketchPad.style.gridTemplateColumns = sliderValue.value;
+    sketchPad.style.gridTemplateRows = sliderValue.value;
+
+    sketchPad.textContent = '';
+
+    for (let index = 0; index < sliderValue.value * sliderValue.value; index++) {
+        const sketchPixel = document.createElement('div');
+        sketchPixel.classList.add('sketchPixel');
+        sketchPad.appendChild(sketchPixel);
+    }
+
+}
+
+changePixelPad();
