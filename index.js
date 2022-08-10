fbe582b67ceeed663dc2695c50e95a344446e55b
ChyenneW <williamscheyenne771@gmail.com>
2022-08-10 06:53:03 -0500

    Display slider count from js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const sketchPad = document.getElementById("sketchPad");
+const sketchPad = document.getElementById('sketchPad');
 
 const sketchPixel = document.createElement('div');
 sketchPixel.setAttribute('style', 'border:5px solid black; background-color: red;');
 
+const sliderValue = document.getElementById('slider');
+const gridSliderCount = document.getElementById('gridSliderCount');
+gridSliderCount.textContent = `${sliderValue.value} x ${sliderValue.value}`;
 
+
+sliderValue.addEventListener('change', (event) => {
+    gridSliderCount.textContent = `${sliderValue.value} x ${sliderValue.value}`;
+});
 
 sketchPad.appendChild(sketchPixel);
