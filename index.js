abd4bece1b02d60469a329adbf45b97fef110fa6
ChyenneW <williamscheyenne771@gmail.com>
2022-08-16 05:06:17 -0500

    Change pixel color when clicked

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ sliderValue.addEventListener('change', (event) => {
 });
 
 const sketchPad = document.getElementById('sketchPad');
+let sketchPixel;
+
+function createPixelId() {
+    let pixelId = `${Math.floor(Math.random() * 100)}-${Math.floor(Math.random() * 100)}`;
+    return pixelId;
+}
 
 function changePixelPad() {
     sketchPad.style.gridTemplateColumns = `repeat(${sliderValue.value}, 1fr)`;
@@ -16,14 +22,28 @@ function changePixelPad() {
     sketchPad.textContent = '';
 
     for (let index = 0; index < sliderValue.value * sliderValue.value; index++) {
-        const sketchPixel = document.createElement('div');
+        sketchPixel = document.createElement('div');
         sketchPixel.classList.add('sketchPixel');
+        sketchPixel.setAttribute('id', createPixelId());
         sketchPad.appendChild(sketchPixel);
-    }
+    };
 
+    sketchPad.addEventListener('click', function (e) {
+        if (e.target.classList.contains('sketchPixel')) {
+            let pixelId = e.target.id;
+            console.log(e.target.id);
+            document.getElementById(pixelId).style.backgroundColor = "red";
+        }
+    });
 }
 
 changePixelPad();
 
-// Color Button
 const colorButton = document.getElementById('colorButton');
+
+function activateColor() {
+    console.log("color mode activated");
+}
+
+colorButton.addEventListener('click', activateColor);
+
