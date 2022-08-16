be8aec6c3016895ff80c50c0f2ac2d75e066ed0e
ChyenneW <williamscheyenne771@gmail.com>
2022-08-16 06:52:56 -0500

    Add dynamic pixel color update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Slider
 const sliderValue = document.getElementById('slider');
 const gridSliderCount = document.getElementById('gridSliderCount');
 gridSliderCount.textContent = `${sliderValue.value} x ${sliderValue.value}`;
@@ -8,6 +9,30 @@ sliderValue.addEventListener('change', (event) => {
     changePixelPad();
 });
 
+// ColorPicker
+const colorSelector = document.getElementById('colorSelector');
+const colorButton = document.getElementById('colorButton');
+let newColor = '#e66465';
+
+colorSelector.addEventListener('change', (event) => {
+    console.log(event.target.value)
+    newColor = event.target.value;
+})
+
+function activeColor(buttonType) {
+    if (buttonType === "colorButton") {
+    } else if (buttonType === "rainbowButton") {
+        console.log("rainbow")
+    } else {
+        console.log("clear");
+    }
+}
+// console.log("color mode activated");
+// colorButton.classList.add('activeButton');
+
+colorButton.addEventListener('click', activeColor);
+
+// SketchPad and Pixels
 const sketchPad = document.getElementById('sketchPad');
 let sketchPixel;
 
@@ -32,18 +57,9 @@ function changePixelPad() {
         if (e.target.classList.contains('sketchPixel')) {
             let pixelId = e.target.id;
             console.log(e.target.id);
-            document.getElementById(pixelId).style.backgroundColor = "red";
+            document.getElementById(pixelId).style.backgroundColor = `${newColor}`;
         }
     });
 }
 
 changePixelPad();
-
-const colorButton = document.getElementById('colorButton');
-
-function activateColor() {
-    console.log("color mode activated");
-}
-
-colorButton.addEventListener('click', activateColor);
-
